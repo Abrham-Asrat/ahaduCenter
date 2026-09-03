7b38fcbe8a8c32b5d481b50e8e245a3790277989
Abrham-Asrat <abrhamasrat29@gmail.com>
2026-09-04 01:06:10 +0300

    refactor: remove unused model files for Book, BorrowRecord, Order, and Product

--- server/models/BorrowRecord.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-const BorrowRecordSchema = new mongoose.Schema({}, { timestamps: true });
-
-module.exports = mongoose.model('BorrowRecord', BorrowRecordSchema);
