56644a65d6584a314dc2e0639aa2a3140d0a8132
Abrham <abrhamasrat29@gmail.com>
2026-09-21 15:28:22 +0300

    feat: enhance CORS support and environment config

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const REQUIRED_VARS = [
   'PORT', 'MONGO_URI', 'JWT_SECRET',
-  'CLIENT_ORIGIN',
+  'CLIENT_ORIGIN', 'CLIENT_URL',
 ];
 const missing = REQUIRED_VARS.filter((key) => !process.env[key]);
 if (missing.length > 0) {
