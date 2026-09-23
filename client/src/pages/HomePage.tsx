45f98527b907f8f362c2a9c928e30337f04b785d
Abrham <abrhamasrat29@gmail.com>
2026-09-24 01:37:31 +0300

    refactor: apply reveal effects to home page layout

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -5,19 +5,32 @@ import HomeHero from '../components/home/HomeHero';
 import HomeProcess from '../components/home/HomeProcess';
 import HomeStats from '../components/home/HomeStats';
 import HomeTestimonials from '../components/home/HomeTestimonials';
+import HomeReveal from '../components/home/HomeReveal';
 import Navbar from '../components/common/Navbar';
 
 const HomePage = () => {
   return (
     <div className="bg-background text-on-surface antialiased selection:bg-primary/30 selection:text-on-primary-container">
       <Navbar />
-      <main className="w-full bg-background">
-        <HomeHero />
-        <HomeStats />
-        <HomeCategories />
-        <HomeFeatured />
-        <HomeProcess />
-        <HomeTestimonials />
+      <main className="w-full bg-background pb-20 md:pb-0">
+        <HomeReveal>
+          <HomeHero />
+        </HomeReveal>
+        <HomeReveal delay={80}>
+          <HomeStats />
+        </HomeReveal>
+        <HomeReveal delay={120}>
+          <HomeCategories />
+        </HomeReveal>
+        <HomeReveal delay={160}>
+          <HomeFeatured />
+        </HomeReveal>
+        <HomeReveal delay={200}>
+          <HomeProcess />
+        </HomeReveal>
+        <HomeReveal delay={240}>
+          <HomeTestimonials />
+        </HomeReveal>
       </main>
 
       <Footer />
